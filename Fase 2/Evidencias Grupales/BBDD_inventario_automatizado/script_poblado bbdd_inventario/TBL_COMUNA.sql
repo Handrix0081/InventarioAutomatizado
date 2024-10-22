04b957654b3a87862e511bb05e853c7341809c98
Hans Messen <113482049+Handrix0081@users.noreply.github.com>
2024-10-22 11:06:27 -0300

    Update TBL_COMUNA.sql

--- Fase 2/Evidencias Grupales/BBDD_inventario_automatizado/script_poblado bbdd_inventario/TBL_COMUNA.sql
+++ Fase 2/Evidencias Grupales/BBDD_inventario_automatizado/script_poblado bbdd_inventario/TBL_COMUNA.sql
@@ -1,8 +1,8 @@
 insert into TBL_COMUNA (IdComuna, NombreComuna, ActivoComuna) values (1, 'Lago dos Rodrigues', true);
-insert into TBL_COMUNA (IdComuna, NombreComuna, ActivoComuna) values (2, 'Lestijärvi', true);
-insert into TBL_COMUNA (IdComuna, NombreComuna, ActivoComuna) values (3, 'Arendal', true);
-insert into TBL_COMUNA (IdComuna, NombreComuna, ActivoComuna) values (4, 'Xiluodu', false);
-insert into TBL_COMUNA (IdComuna, NombreComuna, ActivoComuna) values (5, 'Tapon', false);
+insert into TBL_COMUNA (IdComuna, NombreComuna, ActivoComuna) values (2, 'LLanquihue', true);
+insert into TBL_COMUNA (IdComuna, NombreComuna, ActivoComuna) values (3, 'Valparaiso', true);
+insert into TBL_COMUNA (IdComuna, NombreComuna, ActivoComuna) values (4, 'frutillar', false);
+insert into TBL_COMUNA (IdComuna, NombreComuna, ActivoComuna) values (5, 'isla grande', false);
 insert into TBL_COMUNA (IdComuna, NombreComuna, ActivoComuna) values (6, 'Saint-Germain-en-Laye', false);
 insert into TBL_COMUNA (IdComuna, NombreComuna, ActivoComuna) values (7, 'Dongting', false);
 insert into TBL_COMUNA (IdComuna, NombreComuna, ActivoComuna) values (8, 'Wiśniewo', false);
